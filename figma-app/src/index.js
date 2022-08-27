e7a8ce96d04dc16143b28d7f7b766164dc1f041d
salaxa12 <43007509+salaxa12@users.noreply.github.com>
2022-08-27 21:02:52 +0400

    landing page
    - created landing page
    - wrote it's style
    - gave it responsive design as well

--- figma-app/src/index.js
+++ figma-app/src/index.js
@@ -1,11 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import banner from "../src/media/banner.png";
+import phoneBanner from "../src/media/mobile-banner.png";
+import logo from "../src/media/LOGO.png";
 
 const Test = () => {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="d-flex flex-column align-items-center justify-content-center container main">
+      <img src={logo} alt="logo" className="col-5 landing-logo"></img>
+
+      <picture>
+        <source srcset={phoneBanner} media="(max-width: 390px)" />
+        <img
+          src={banner}
+          alt="banner"
+          className="col-lg-5 col-md-8 col-sm-11 landing-banner"
+        ></img>
+      </picture>
+      <div className="d-flex flex-column col-lg-3 col-md-5 col-sm-12 col-xs-12 landing-button-area">
+        <button type="button" class="landing-button">
+          ჩანაწერის დამატება
+        </button>
+        <button type="button" class="landing-button">
+          ჩანაწერების სია
+        </button>
+      </div>
     </div>
   );
 };
